00410f7db36c0d819235db004fbcc2f19b366b18
HelenCris <79730539+HelenCris@users.noreply.github.com>
2022-01-18 19:09:47 -0300

    dicee game

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+var randomNumber2 = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+
+//Random images of the dice
+document.getElementById("img1").src = `images/dice${randomNumber1}.png`;
+document.getElementById("img2").src = `images/dice${randomNumber2}.png`;
+
+//Change the names 
+
+if (randomNumber1 < randomNumber2) { 
+    document.getElementById("h1").innerHTML = "Dicee 1 Wins!";
+} else if (randomNumber1 === randomNumber2) { 
+    document.getElementById("h1").innerHTML = "Draw!";
+} else if (randomNumber2 < randomNumber1) { 
+    document.getElementById("h1").innerHTML = "Dicee 2 Wins!";
+}
